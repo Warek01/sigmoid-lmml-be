b85a4bf5f99081987f38b8f6f37b8c360cde9cb3
Alex Dobrojan <alex.dobrojan@smartdata.solutions>
2025-04-02 11:29:09 +0300

    Run format

--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -4,28 +4,26 @@ import {
   Injectable,
   Logger,
   NestInterceptor,
-} from '@nestjs/common';
-import { Observable, tap } from 'rxjs';
+} from '@nestjs/common'
+import { Observable, tap } from 'rxjs'
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger = new Logger(LoggingInterceptor.name);
+  private readonly logger = new Logger(LoggingInterceptor.name)
 
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    const startTime = Date.now();
-    const request = context.switchToHttp().getRequest();
-    const { method, url } = request;
+    const startTime = Date.now()
+    const request = context.switchToHttp().getRequest()
+    const { method, url } = request
 
     return next.handle().pipe(
       tap(() => {
-        const endTime = Date.now();
-        this.logger.log(
-          `HTTP ${method} ${url} took ${endTime - startTime}ms`,
-        );
+        const endTime = Date.now()
+        this.logger.log(`HTTP ${method} ${url} took ${endTime - startTime}ms`)
       }),
-    );
+    )
   }
 }
